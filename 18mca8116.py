b9f1ac4a34bd673460bc97e96657586307c111c0
Jaikishan <62866640+jaisharma13@users.noreply.github.com>
2020-03-30 22:19:02 +0530

    Add files via upload

---
+++ 18mca8116.py
@@ -0,0 +1,25 @@
+import urllib
+import cv2,time
+import numpy as np
+first_frame = None
+
+url= ’http://192.168.2.52:8080/abc.jpg’
+imgResp = urllib.urlopen(url)
+imgNp = np.array(bytearray(imgResp.read()),dtype=np.uint8)
+img = cv2.imdecode(imgNp,-1)
+cv2.namedWindow(‘image’,cv2.WINDOW_NORMAL)
+cv2.resizeWindow(‘image’,800,800)
+
+i=0
+while True:
+	imgResp=urllib.urlopen(url)
+	imgNp=np.array(bytearray(imgResp.read()),dtype=n.uint8)
+	img=cv2.imdecode(imgNp,-1)
+	cv2.imshow(‘abc’,img)
+	cv2.imwrite(‘test.jpg’’,img)
+	key=cv2.waitKey(1)
+	if key==ord(‘q’):
+		break
+
+cv2.destroyAllWindows
+
